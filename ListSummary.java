96041f48184c79bbed49c6a46823b8ee24a4ed49
Horizon Gallery Coding Research <info.calmalgo@gmail.com>
2021-05-26 22:01:46 -0400

    Add files via upload

--- ListSummary.java
+++ ListSummary.java
@@ -7,7 +7,7 @@ public class ListSummary {
 
     private final List<Double> doubleList;
 
-    ListSummary (List<Double> doubleList) {
+    ListSummary(List<Double> doubleList) {
         this.doubleList = doubleList;
     }
 
@@ -15,71 +15,72 @@ public class ListSummary {
         return doubleList;
     }
 
-    OptionalDouble findArithmeticMean () {
-        Function<List<Double>, OptionalDouble> arithmeticMean = list -> {
-            if (list.isEmpty() || list.contains(null))
-                return OptionalDouble.empty();
-            return list.stream().mapToDouble(Double::doubleValue).average();
-        };
+    Function<List<Double>, OptionalDouble> arithmeticMean = list -> {
+        if (list.isEmpty() || list.contains(null))
+            return OptionalDouble.empty();
+        return list.stream().mapToDouble(Double::doubleValue).average();
+    };
+
+    Function<List<Double>, OptionalDouble> geometricMean = list -> {
+        if (list.isEmpty() || list.contains(null))
+            return OptionalDouble.empty();
+        double multiplier = list.stream().reduce(1d, (i, r) -> i * (1d + r / 100d), (a, b) -> a * b);
+        return OptionalDouble.of((    Math.pow(multiplier, 1d / list.size())   -   1d) * 100d);
+    };
+
+    Function<List<Double>, OptionalDouble> median = list -> {
+
+        if (list.isEmpty() || list.contains(null))
+            return OptionalDouble.empty();
+
+        DoubleStream sortedStream = list.stream()
+                .mapToDouble(Double::doubleValue)
+                .sorted();
+
+        return list.size() % 2 == 0 ?
+                sortedStream.skip((list.size() / 2) - 1).limit(2).average() :
+                sortedStream.skip(list.size() / 2).findFirst();
+    };
+
+    Function<List<Double>, OptionalDouble> maximum = list -> {
+        if (list.isEmpty() || list.contains(null))
+            return OptionalDouble.empty();
+        return list.stream().mapToDouble(Double::doubleValue).max();
+    };
+
+    Function<List<Double>, OptionalDouble> minimum = list -> {
+        if (list.isEmpty() || list.contains(null))
+            return OptionalDouble.empty();
+        return list.stream().mapToDouble(Double::doubleValue).min();
+    };
+
+    Function<List<Double>, OptionalDouble> sum = list -> {
+        if (list.isEmpty() || list.contains(null))
+            return OptionalDouble.empty();
+        return OptionalDouble.of(list.stream().mapToDouble(Double::doubleValue).sum());
+    };
+
+    OptionalDouble findArithmeticMean() {
         return arithmeticMean.apply(doubleList);
     }
 
-
     OptionalDouble findGeometricMeanPercentage() {
-        // List<Double> is a list of percentage numbers
-        Function<List<Double>, OptionalDouble> geometricMean = list -> {
-            if (list.isEmpty() || list.contains(null))
-                return OptionalDouble.empty();
-            double multiplier = list.stream().reduce(1d, (i, r) -> i * (1d + r / 100d), (a, b) -> a * b);
-            return OptionalDouble.of((    Math.pow(multiplier, 1d / list.size())   -   1d) * 100d);
-        };
         return geometricMean.apply(doubleList);
     }
 
-
     OptionalDouble findMedian() {
-        Function<List<Double>, OptionalDouble> median = list -> {
-
-            if (list.isEmpty() || list.contains(null))
-                return OptionalDouble.empty();
-
-            DoubleStream sortedStream = list.stream()
-                    .mapToDouble(Double::doubleValue)
-                    .sorted();
-
-            return list.size() % 2 == 0 ?
-                    sortedStream.skip((list.size() / 2) - 1).limit(2).average() :
-                    sortedStream.skip(list.size() / 2).findFirst();
-        };
         return median.apply(doubleList);
     }
 
-
     OptionalDouble findMaximum() {
-        Function<List<Double>, OptionalDouble> maximum = list -> {
-            if (list.isEmpty() || list.contains(null))
-                return OptionalDouble.empty();
-            return list.stream().mapToDouble(Double::doubleValue).max();
-        };
         return maximum.apply(doubleList);
     }
 
-
     OptionalDouble findMinimum() {
-        Function<List<Double>, OptionalDouble> minimum = list -> {
-            if (list.isEmpty() || list.contains(null))
-                return OptionalDouble.empty();
-            return list.stream().mapToDouble(Double::doubleValue).min();
-        };
         return minimum.apply(doubleList);
     }
 
     OptionalDouble findSum() {
-        Function<List<Double>, OptionalDouble> sum = list -> {
-            if (list.isEmpty() || list.contains(null))
-                return OptionalDouble.empty();
-            return OptionalDouble.of(list.stream().mapToDouble(Double::doubleValue).sum());
-        };
         return sum.apply(doubleList);
     }
 
--- Main.java
+++ Main.java
@@ -5,12 +5,38 @@ public class Main {
 
     public static void main (String... args) {
 
-List<Double> doubleList = Arrays.asList(5d, 6d, 1d, 2d, 3d, 4d, 7d, 8d);
+        List<Double> doubleList;
 
-ListStats listStats = new ListStats(doubleList);
-System.out.print("Median is: ");
-listStats.findMedian()
-        .ifPresentOrElse(System.out::println, () -> System.out.println("not available"));
+        doubleList = Arrays.asList(5d, 6d, 1d, 2d, 3d, 4d, 7d);
+        //doubleList = Arrays.asList(5d, 6d, 1d, 2d, 3d, null, 7d);
+        //doubleList = Collections.emptyList();
+
+        ListSummary listSummary = new ListSummary(doubleList);
+
+
+        System.out.print("Arithmetic mean is: ");
+        listSummary.findArithmeticMean()
+                .ifPresentOrElse(System.out::println, () -> System.out.println("not available"));
+
+        System.out.print("Geometric mean percentage is: ");
+        listSummary.findGeometricMeanPercentage()
+                .ifPresentOrElse(System.out::println, () -> System.out.println("not available"));
+
+        System.out.print("Median is: ");
+        listSummary.findMedian()
+                .ifPresentOrElse(System.out::println, () -> System.out.println("not available"));
+
+        System.out.print("Maximum is: ");
+        listSummary.findMaximum()
+                .ifPresentOrElse(System.out::println, () -> System.out.println("not available"));
+
+        System.out.print("Minimum is: ");
+        listSummary.findMinimum()
+                .ifPresentOrElse(System.out::println, () -> System.out.println("not available"));
+
+        System.out.print("Sum is: ");
+        listSummary.findSum()
+                .ifPresentOrElse(System.out::println, () -> System.out.println("not available"));
 
     }
 
