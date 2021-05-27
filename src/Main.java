fc7517d5ae4b4c5888a34c73bc38bcd955a5cecd
Anthony W <peacefulmindgallery@yahoo.com>
2021-05-26 23:42:25 -0400

    Variance and Standard Deviation

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,9 @@ public class Main {
         doubleList = Arrays.asList(5d, 6d, 1d, 2d, 3d, 4d, 7d);
         //doubleList = Arrays.asList(5d, 6d, 1d, 2d, 3d, null, 7d);
         //doubleList = Collections.emptyList();
+        //doubleList = Arrays.asList(5d);
 
         ListSummary listSummary = new ListSummary(doubleList);
-
 
         System.out.print("Arithmetic mean is: ");
         listSummary.findArithmeticMean()
@@ -38,6 +38,21 @@ public class Main {
         listSummary.findSum()
                 .ifPresentOrElse(System.out::println, () -> System.out.println("not available"));
 
-    }
+        System.out.print("Population Variance is: ");
+        listSummary.findVariance(listSummary.TYPE_POPULATION)
+                .ifPresentOrElse(System.out::println, () -> System.out.println("not available"));
 
+        System.out.print("Population Standard Deviation is: ");
+        listSummary.findStandardDeviation(listSummary.TYPE_POPULATION)
+                .ifPresentOrElse(System.out::println, () -> System.out.println("not available"));
+
+        System.out.print("Sample Variance is: ");
+        listSummary.findVariance(listSummary.TYPE_SAMPLE)
+                .ifPresentOrElse(System.out::println, () -> System.out.println("not available"));
+
+        System.out.print("Sample Standard Deviation is: ");
+        listSummary.findStandardDeviation(listSummary.TYPE_SAMPLE)
+                .ifPresentOrElse(System.out::println, () -> System.out.println("not available"));
+
+    }
 }
